6d4d4c7696dd8103f0920703d4ef5c2cef88e280
Nompumelelo Hlongwane <61636340+Nompumelelo-np@users.noreply.github.com>
2024-03-08 18:55:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class PalindromeNumberExample
+{
+
+    public static void Main(string[] args)
+    {
+        int num, rev, sum = 0, temp;
+        Console.Write("Enter number: ");
+        num = int.Parse(Console.ReadLine());
+        temp = num;
+        while (num > 0)
+        {
+            rev = num % 10;
+            sum = (sum * 10) + rev;
+            num = num / 10;
+        }
+        if (temp == sum)
+            Console.Write(" The given number is Palindrome.");
+        else
+            Console.Write(" The given number is not Palindrome.");
+    }
+}
